21c48a114d339d6d21f1f5c6cf6ac5e4c35c523c
Teilnehmer <thorsten.daempfert@gmail.com>
2020-03-04 10:54:12 +0100

    2020-03-04 clean up

--- OnTrace.Easy/Easy.Logger.Extensions/Properties/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Easy.Logger.Tests.Unit")]
--- OnTrace.Easy/Easy.Logger/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Easy.Logger.Extensions")]
-[assembly: InternalsVisibleTo("Easy.Logger.Tests.Unit")]
